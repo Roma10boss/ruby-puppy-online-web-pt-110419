90eea94bd0a625f594e4013952e779b846b7681d
Romario Gustave <romagustave10@gmail.com>
2019-12-09 22:42:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,18 +8,10 @@ class Dog
   end
   
   def self.all
-    @all.each do |dog|
+    @@all.each do |dog|
       puts dog.name 
     end
   end 
-    def self.print_all 
-      @@all 
-      puts dog.name 
-    end 
-    
-    def save 
-      @all << self 
-    end 
     
     def self.clear_all
       @@all.clear
