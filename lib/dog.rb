79b0b4b7af1ec02ec2c3341c10c0cbf40315ed46
Romario Gustave <romagustave10@gmail.com>
2019-12-09 22:32:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,25 @@
 # Add your code here
 class Dog 
-  attr_accessor 
+  attr_accessor :name
+  @@all = []
+  def initialize(name)
+    @name = name 
+    save 
+  end 
   
-  def initialize
+  def self.all 
+    @all
+    end
     
-  
+    def self.print_all 
+      @@all 
+    end 
+    
+    def save 
+      @all << self 
+    end 
+    
+    def self.clear_all
+      @all =[]
+    end 
 end
